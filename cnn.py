4a523dbee077eedc26fd6d004ce5f2dcedadfb79
Deepika <32121254+Deepi20@users.noreply.github.com>
2019-06-30 23:51:48 -0500

    Update cnn.py

--- cnn.py
+++ cnn.py
@@ -1,3 +1,13 @@
+import tensorflow as tf
+import numpy as np
+from tensorflow import keras
+mnist = tf.keras.datasets.fashion_mnist
+(training_images, training_labels),(testing_images,testing_labels) = mnist.load_data()
+import matplotlib.pyplot as plt
+print(training_labels[0])
+print(training_images[0])
+training_images = training_images/255.0
+testing_images = testing_images/255.0
 class myCallback(tf.keras.callbacks.Callback):
   def on_epoch_end(self, epoch, logs={}):
     if(logs.get('accuracy')>0.91):
