dff95d67f3b390bbf7129d95338359864302eff1
Deepika <32121254+Deepi20@users.noreply.github.com>
2019-07-15 23:53:36 -0500

    Update nn.py

--- nn.py
+++ nn.py
@@ -11,10 +11,5 @@ testing_images = testing_images/255.0
 model = tf.keras.models.Sequential([tf.keras.layers.Flatten(),
                                    tf.keras.layers.Dense(128,activation = tf.nn.relu),
                                    tf.keras.layers.Dense(10,activation = tf.nn.softmax)])
-model.compile(optimizer = tf.keras.optimizers.Adam(),
-             loss = 'sparse_categorical_crossentropy',
-             metrics = ['accuracy'])
-model.fit(training_images,training_labels,epochs = 100)
-model.evaluate(testing_images, testing_labels)
 
 
