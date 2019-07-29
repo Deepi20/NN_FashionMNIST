13f369c380f6f333334892016d241ff1ed011910
Deepika <32121254+Deepi20@users.noreply.github.com>
2019-07-28 23:17:38 -0500

    Update usingpytorch.py

--- usingpytorch.py
+++ usingpytorch.py
@@ -79,3 +79,21 @@ ps = torch.exp(logps)
 probab = list(ps.numpy()[0])
 print("Predicted Digit =", probab.index(max(probab)))
 view_classify(img.view(1, 28, 28), ps)
+correct_count, all_count = 0, 0
+for images,labels in valloader:
+  for i in range(len(labels)):
+    img = images[i].view(1, 784)
+    with torch.no_grad():
+        logps = model(img)
+
+    
+    ps = torch.exp(logps)
+    probab = list(ps.numpy()[0])
+    pred_label = probab.index(max(probab))
+    true_label = labels.numpy()[i]
+    if(true_label == pred_label):
+      correct_count += 1
+    all_count += 1
+
+print("Number Of Images Tested =", all_count)
+print("\nModel Accuracy =", (correct_count/all_count))
