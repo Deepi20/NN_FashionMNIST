819a28c1982f1e4eced0cd84922e0b4423c7091a
Deepika <32121254+Deepi20@users.noreply.github.com>
2019-09-20 00:00:07 -0500

    Update usingpytorch.py

--- usingpytorch.py
+++ usingpytorch.py
@@ -15,16 +15,19 @@ trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True)
 valloader = torch.utils.data.DataLoader(valset, batch_size=64, shuffle=True)
 dataiter = iter(trainloader)
 images, labels = dataiter.next()
-
 print(images.shape)
 print(labels.shape)
 
-
-
-
-
-
-
+plt.imshow(images[0].numpy().squeeze(), cmap='gray_r');
+figure = plt.figure()
+num_of_images = 60
+for index in range(1, num_of_images + 1):
+    plt.subplot(6, 10, index)
+    plt.axis('off')
+    plt.imshow(images[index].numpy().squeeze(), cmap='gray_r')
+input_size = 784
+hidden_sizes = [128, 64]
+output_size = 10
 model = nn.Sequential(nn.Linear(input_size, hidden_sizes[0]),
                       nn.ReLU(),
                       nn.Linear(hidden_sizes[0], hidden_sizes[1]),
