1591ac396ff84202ca8456646586ea7fc7df19b3
Deepika <32121254+Deepi20@users.noreply.github.com>
2019-09-20 00:01:10 -0500

    Update usingpytorch.py

--- usingpytorch.py
+++ usingpytorch.py
@@ -5,11 +5,18 @@ import matplotlib.pyplot as plt
 from time import time
 from torchvision import datasets, transforms
 from torch import nn, optim
+trainset = datasets.MNIST('PATH_TO_STORE_TRAINSET', download=True, train=True, transform=transform)
+
+valset = datasets.MNIST('PATH_TO_STORE_TESTSET', download=True, train=False, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True)
+valloader = torch.utils.data.DataLoader(valset, batch_size=64, shuffle=True)
+dataiter = iter(trainloader)
+images, labels = dataiter.next()
+print(images.shape)
+print(labels.shape)
 transform = transforms.Compose([transforms.ToTensor(),
                               transforms.Normalize((0.5,), (0.5,)),
                               ])
-
-
 plt.imshow(images[0].numpy().squeeze(), cmap='gray_r');
 figure = plt.figure()
 num_of_images = 60
